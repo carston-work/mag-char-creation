1912e5ecccd5c307a117efd9ca2f7675c01d2e68
Carston Work <carstonwork.dev@gmail.com>
2021-11-11 14:32:30 -0700

    Crew owners can now accept or decline requests to join their crew.

--- app/crews/views.py
+++ app/crews/views.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, render_template, url_for, redirect
+from flask import Blueprint, render_template, url_for, redirect, current_app, request as req
 from flask.helpers import flash
 from flask_login import login_required, current_user
 from app.models import Crew, Request
 from app.crews.forms import CrewForm, RequestForm
 from app import db
+import json
 
 crew = Blueprint('crew', __name__, template_folder="templates")
+app = current_app
 
 @crew.route('/create', methods=["GET", "POST"])
 @login_required
@@ -15,28 +17,32 @@ def add_crew():
     if form.validate_on_submit():
 
         name = form.name.data
-        seeking = form.seeking.data
-        owner_id = current_user.user_id
+        crew_exists = Crew.query.filter_by(name=name).first()
+        if crew_exists:
+            flash("That Crew name is taken. Please use a different one.")
+        else:
+            seeking = form.seeking.data
+            owner_id = current_user.user_id
 
-        preferences = form.preferences.data
-        preferences_int = 0
-        if 'action' in preferences:
-            preferences_int += 16
-        if "adventure" in preferences:
-            preferences_int += 8
-        if 'side-quests' in preferences:
-            preferences_int += 4
-        if 'character-driven' in preferences:
-            preferences_int += 2
-        if 'plot-driven' in preferences:
-            preferences_int += 1
+            preferences = form.preferences.data
+            preferences_int = 0
+            if 'action' in preferences:
+                preferences_int += 16
+            if "adventure" in preferences:
+                preferences_int += 8
+            if 'side-quests' in preferences:
+                preferences_int += 4
+            if 'character-driven' in preferences:
+                preferences_int += 2
+            if 'plot-driven' in preferences:
+                preferences_int += 1
 
-        new_crew = Crew(owner_id, name, seeking, preferences_int)
+            new_crew = Crew(owner_id, name, seeking, preferences_int)
 
-        db.session.add(new_crew)
-        db.session.commit()
+            db.session.add(new_crew)
+            db.session.commit()
 
-        return redirect(url_for("users.user_profile", username=current_user.username))
+            return redirect(url_for("users.user_profile", username=current_user.username))
     
     return render_template("add_crew.html", form=form)
 
@@ -73,3 +79,31 @@ def send_request(crew_id):
         return redirect(url_for('users.user_profile', username=current_user.username))
     
     return render_template('send_request.html', form=form)
+
+
+@crew.route('/view_crew/<crew_id>')
+def view_crew(crew_id):
+    crew = Crew.query.get(crew_id)
+    if crew:
+        return render_template('view_crew.html', crew=crew)
+    else:
+        flash("That crew doesn't exist")
+        return redirect(url_for('main.home'))
+
+
+@crew.route('/requests/<request_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_request_message(request_id):
+    my_request = Request.query.get(request_id)
+    if my_request:
+        if req.method == "PUT":
+            my_request.character.crew_id = my_request.crew.crew_id
+            db.session.delete(my_request)
+        elif req.method == "DELETE":
+            db.session.delete(my_request)
+        else:
+            return json.dumps(my_request.message)
+
+        db.session.commit()
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+    return json.dumps({'success':False}), 400, {'ContentType':'application/json'}
